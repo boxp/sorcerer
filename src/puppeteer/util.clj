2da05211c6f2ae295c82df69fc0adb2c655bc1a1
boxp <tiyotiyouda@gmail.com>
2017-12-17 22:51:50 +0900

    Add util.clj

---
+++ src/puppeteer/util.clj
@@ -0,0 +1,19 @@
+(ns puppeteer.util
+  (:require [clojure.string :as string]))
+
+(defn- sanitize-name
+  [name]
+  (string/replace name #"[\\\/\_\.]" "-"))
+
+(defn ->app
+  [{:keys [repo-name branch-name] :as job}]
+  (str (sanitize-name repo-name) "-" (sanitize-name branch-name)))
+
+(defn ->host
+  [{:keys [domain repo-name branch-name]}]
+  (str (sanitize-name repo-name) "-" (sanitize-name branch-name) "." domain))
+
+(defn ->service-name
+  [{:keys [repo-name branch-name] :as job}]
+  (str (sanitize-name repo-name) "-" (sanitize-name branch-name)))
+
