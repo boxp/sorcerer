a1f2d85ea6a2c7b70f4e495358873227b0e7ad75
Keitaro Takeuchi <tiyotiyouda@gmail.com>
2018-05-01 19:55:22 +0900

    :fire: Feature/replace circleci to gccb (#8)
    * :sparkles: Replace CircleCI to gccb
    
    * :bug: Add envsubst docker image
    
    * :bug: Fix cluster name
    
    * :bug: Add use_client_certificate
    
    * :bug: Add custom volume for build steps
    
    * :bug: Fix yml syntax
    
    * :bug: Add entrypoint
    
    * :bug: bash -> sh
    
    * :bug: Add env to envsubst build step
    
    * :bug: Fix docker image domain
    
    * :books: Fix README.md
    
    * :green_heart: Reborn circle.yml
    
    * :green_heart: Reborn deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ for f in k8s/*.yml
 do
 	envsubst < $f > "generated-$(basename $f)"
 done
-sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/puppeteer:$CIRCLE_SHA1
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/puppeteer:$CIRCLE_SHA1
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl apply -f generated-deployment.yml
